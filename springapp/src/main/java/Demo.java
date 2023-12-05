48f13d193acb1dde615e1d1deec75f8d46fa0acf
Iamneo <techadmin@iamneo.ai>
2023-12-05 10:04:16 +0000

    Auto-save-commit-12/5/2023--3:34:11-PM

--- springapp/src/main/java/Demo.java
+++ springapp/src/main/java/Demo.java
@@ -2,7 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("welcome");
         System.out.println("qwerty");
-        
+        System.out.println();
     }
     
 }
